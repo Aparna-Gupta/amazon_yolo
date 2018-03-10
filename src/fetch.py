cc6e6b935ce8e13858a53f0fea64d068cd9a23f7
Pratik Anand <pratik.preet@gmail.com>
2018-03-09 21:59:59 -0500

    data retrieval complete

--- src/fetch.py
+++ src/fetch.py
@@ -2,7 +2,7 @@ from amazon_api import *
 import json
 from pprint import pprint
 
-DELAY = .5    # taaki Amazon danda naa maare
+DELAY = .7    # taaki Amazon danda naa maare
 
 '''
 It will iterate through all the 2014 data entries and retrieve latest 2018 product data
@@ -11,8 +11,20 @@ from Amazon API and add it back to the data
 '''
 def iter_retrive_data ( amazon_obj, asin_product_dict ) :
     asin_not_found_list = []
+    asin_product_salesrank = {}
+    SKIP_ASIN = "B005FM2B5K"
+    skip_flag = True
+    ctr =  0
     for index, asin in enumerate ( asin_product_dict ) :
         print ( " {} ASIN : {}".format ( index, asin  )  )
+        if skip_flag is True :
+            if asin != SKIP_ASIN :
+                print ( "skipping ", asin )
+                continue
+            else :
+                skip_flag = False
+                continue   #continue after this asin
+
         util_delay ( DELAY )
         data = get_data ( amazon_obj, asin )
         if data is None :
@@ -21,13 +33,17 @@ def iter_retrive_data ( amazon_obj, asin_product_dict ) :
             asin_not_found_list.append ( asin )
             continue
         description, rank = data
-        asin_product_dict[ asin ]['description'] = description
-        asin_product_dict[ asin ]['salesrank_2018'] = rank
-        # pprint ( asin_product_dict[ asin ] )
+        asin_product_salesrank[ asin ] = {'description' : description, 'salesrank_2018' : rank }
+        ctr += 1
+        if ctr % 1000 == 0 :
+            with open('../partial-{}.json'.format(ctr)  , 'w') as outfile:
+                json.dump(asin_product_salesrank, outfile)
+                asin_product_salesrank = {}  # reset dictionary
 
-    for asin in asin_not_found_list :
-        del asin_product_dict[ asin ]
-        print( "Deleted entry for asin not found" )
+
+    # for asin in asin_not_found_list :
+    #     del asin_product_dict[ asin ]
+    #     print( "Deleted entry for asin not found" )
 
 
 def get_data ( amazon_obj, product_id ) :
@@ -48,5 +64,5 @@ if __name__ == '__main__' :
     amazon_obj = authentication(  )
     iter_retrive_data ( amazon_obj, asin_product_dict )
     
-    with open('../asinReviewDict_v2_modified.json', 'w') as outfile:
-        json.dump(asin_product_dict, outfile)
+    # with open('../asinReviewDict_v2_modified.json', 'w') as outfile:
+    #     json.dump(asin_product_dict, outfile)
